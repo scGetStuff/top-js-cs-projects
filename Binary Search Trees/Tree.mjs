0e1761ecc8fc31ba16f2449d91c7df44932fece8
Scott <67354101+scGetStuff@users.noreply.github.com>
2022-10-10 20:21:47 -0500

    work in progress

--- Binary Search Trees/Tree.mjs
+++ Binary Search Trees/Tree.mjs
@@ -1,54 +1,120 @@
-import { Node } from './Node.mjs';
+import { Node } from "./Node.mjs";
 
 const cl = console.log;
 
 class Tree {
     constructor(arr = []) {
-        arr = arr.sort((a, b) => a - b);
         arr = Array.from(new Set(arr));
-        this.root = this.buildTree(arr);
+        arr.sort((a, b) => a - b);
+        this.root = buildTree(arr);
     }
 
-    buildTree(arr = []) {
-        if (arr.length === 0) return null;
+    insert(value) {
+        const newNode = new Node(value);
+        appendLeaf(this.root, newNode);
+    }
 
-        const mid = Math.floor(arr.length / 2);
-        const node = new Node(arr[mid]);
-        const leftArr = arr.slice(0, mid);
-        const rightArr = arr.slice(mid + 1);
+    delete(value) {
+        // TODO: root
+        if (this.root.data === value) return;
 
-        node.left = this.buildTree(leftArr);
-        node.right = this.buildTree(rightArr);
+        let parent = this.root;
 
+        // find the parent node
+        while (parent) {
+            if (isChild(parent, value)) break;
+            parent = nextNode(parent, value);
+        }
+        if (!parent) return;
+
+        // TODO: first pass, dumb delete, add real cases
+        if (parent.left?.data === value) parent.left = null;
+        if (parent.right?.data === value) parent.right = null;
+    }
+
+    find(value) {
+        let node = this.root;
+
+        while (node) {
+            if (node.data === value) break;
+            node = nextNode(node, value);
+        }
         return node;
     }
 
-    dump(node = this.root) {
-        if (node == null) return;
+    levelOrder() {}
 
-        cl('\n');
-        cl(node.data);
-        cl(`${node.left}\t${node.right}`);
-        this.dump(node.left);
-        this.dump(node.right);
-    }
+    inorder() {}
 
-    prettyPrint(node = this.root, prefix = '', isLeft = true) {
+    preorder() {}
+
+    postorder() {}
+
+    height() {}
+
+    depth() {}
+
+    isBalanced() {}
+
+    rebalance() {}
+
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
         if (node.right !== null) {
             this.prettyPrint(
                 node.right,
-                `${prefix}${isLeft ? '│   ' : '    '}`,
+                `${prefix}${isLeft ? "│   " : "    "}`,
                 false
             );
         }
-        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
         if (node.left !== null) {
             this.prettyPrint(
                 node.left,
-                `${prefix}${isLeft ? '    ' : '│   '}`,
+                `${prefix}${isLeft ? "    " : "│   "}`,
                 true
             );
         }
+    }
+}
+
+// TODO: don't know if this is good or bad
+// these utility functions are specific to the class,
+// but do not use the state at all; are basicaly pure functions
+// I hate the stupid #method() and this.#method() syntax
+function nextNode(node, value) {
+    if (value < node.data) return node.left;
+    if (value > node.data) return node.right;
+}
+
+function isChild(parent, value) {
+    return value === parent.left?.data || value === parent.right?.data;
+}
+
+function buildTree(arr = []) {
+    if (arr.length === 0) return null;
+
+    const mid = Math.floor(arr.length / 2);
+    const node = new Node(arr[mid]);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid + 1);
+
+    node.left = buildTree(leftArr);
+    node.right = buildTree(rightArr);
+
+    return node;
+}
+
+function appendLeaf(curNode, newNode) {
+    // ignore duplicate
+    if (newNode.data === curNode.data) return;
+
+    // when leaf found, do insert
+    if (newNode.data < curNode.data) {
+        if (curNode.left) appendLeaf(curNode.left, newNode);
+        else curNode.left = newNode;
+    } else if (newNode.data > curNode.data) {
+        if (curNode.right) appendLeaf(curNode.right, newNode);
+        else curNode.right = newNode;
     }
 }
 
--- Binary Search Trees/script.mjs
+++ Binary Search Trees/script.mjs
@@ -1,15 +1,27 @@
-'use strict';
+"use strict";
 
-import { Tree } from './Tree.mjs';
+import { Tree } from "./Tree.mjs";
 
 console.clear();
 const cl = console.log;
 
 function doStuff() {
-    const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-    // const tree = new Tree([1, 2, 3, 4, 5]);
-    // tree.dump();
-    tree.prettyPrint();
+    {
+        const tree = new Tree([
+            1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,
+        ]);
+        // tree.prettyPrint();
+    }
+    {
+        const tree = new Tree([1, 2, 3, 4, 5]);
+        // tree.insert(7);
+        tree.prettyPrint();
+        // cl(tree.find(3)?.toString());
+        tree.delete(1);
+        tree.prettyPrint();
+    }
+
+
 }
 
 doStuff();
